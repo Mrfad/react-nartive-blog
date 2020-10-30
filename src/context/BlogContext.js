2f66f26c9d90bca0ab6cc1c315bd641b415695bc
mrfad <fady.ghalayiny@outlook.com>
2020-10-30 09:37:23 +0200

    working with context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,15 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({children}) => {
-    return <BlogContext.Provider>
+    const blogPosts = [
+        {title: 'blog Post #1'},
+        {title: 'blog Post #2'},
+        {title: 'blog Post #3'},
+        {title: 'blog Post #4'},
+        {title: 'blog Post #5'}
+    ];
+    return <BlogContext.Provider value={blogPosts}>
         {children}
     </BlogContext.Provider>
 };
+export default BlogContext;
